e7c8802ce2c9a1bbf7adb4f7a88647c6fb50f055
Nethmi Rashmika <h.u.g.nethmirashmika@gmail.com>
2025-06-04 22:14:12 +0530

    update menu_section.py
    add method for automate menu section functionality

---
+++ SeleniumPytest_Assignment/Assignment1/pages/menu_section.py
@@ -0,0 +1,32 @@
+from locators.menu_locator import MenuLocator
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class menu_section:
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def open_menu(self):
+        self.driver.find_element(*MenuLocator.Menu_button).click()
+
+    def close_menu(self):
+        self.driver.find_element(*MenuLocator.Menu_close_button).click()
+
+    def is_menu_display(self):
+        return WebDriverWait(self.driver, 5 ).until(EC.visibility_of_element_located(MenuLocator.Slidebar_menu))
+
+    def click_all_item(self):
+        self.driver.find_element(*MenuLocator.All_item_text).click()
+
+    def click_about(self):
+        self.driver.find_element(*MenuLocator.About_text).click()
+
+
+    def click_logout(self):
+        self.driver.find_element(*MenuLocator.Logout_text).click();
+
+    def click_reset_app_state(self):
+        self.driver.find_element(*MenuLocator.Reset_text).click()
+
+        
