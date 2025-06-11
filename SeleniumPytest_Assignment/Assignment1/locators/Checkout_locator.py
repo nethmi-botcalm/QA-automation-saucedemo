0c18f6be5cc016671efd0c19bc7048d76675ff19
Nethmi Rashmika <h.u.g.nethmirashmika@gmail.com>
2025-06-11 22:30:48 +0530

    Update Checkout_locator.py
    add locators for checkout pages

---
+++ SeleniumPytest_Assignment/Assignment1/locators/Checkout_locator.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.by import By
+
+class checkoutLocators:
+    first_name = (By.ID, "first_name")
+    second_name = (By.ID, "last-name")
+    postal_code = (By.ID, "postal-code")
+    continue_btn = (By.ID, "continue")
+
+    summary_product = (By.CLASS_NAME, "cart_item")
+    product_name = (By.CLASS_NAME, "inventory_item_name")
+    product_desc = (By.CLASS_NAME, "inventory_item_desc")
+    product_price = (By.CLASS_NAME, "inventory_item_price")
+    product_quantity = (By.CLASS_NAME, "cart_quantity")
+    item_total = (By.CLASS_NAME, "//*[@id='checkout_summary_container']/div/div[2]/div[6]")
+    tax = (By.CLASS_NAME, "summary_tax_label")
+    total = (By.CLASS_NAME, "summary_total_label")
+    finish_button = (By.ID, "finish")
+
+    complete_title = (By.CLASS_NAME, "complete-header")
+    complete_text = (By.CLASS_NAME, "complete-text")
